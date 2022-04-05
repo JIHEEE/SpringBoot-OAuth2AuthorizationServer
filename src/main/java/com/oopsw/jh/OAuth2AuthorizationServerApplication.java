8f7035f910b1b731334daf50f06f522a839aecd2
JiheeChoi <JiheeChoi@DESKTOP-MNNAU68>
2022-04-05 14:53:18 +0900

    test commit

--- src/main/java/com/oopsw/jh/OAuth2AuthorizationServerApplication.java
+++ src/main/java/com/oopsw/jh/OAuth2AuthorizationServerApplication.java
@@ -8,6 +8,9 @@ public class OAuth2AuthorizationServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OAuth2AuthorizationServerApplication.class, args);
+	
+		System.out.println("Hello World!");
 	}
-
+	
+	
 }
